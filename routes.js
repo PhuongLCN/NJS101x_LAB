f7ed6c4f8ed4f6c410d8e5e88a7dee703585c089
Maruko Phuong <lcnphuong@gmail.com>
2022-03-16 16:27:11 +0900

    Lab 1.8:  Sử dụng Nodemon để khởi động tự động

--- routes.js
+++ routes.js
@@ -9,6 +9,7 @@ const requestHandler = (req, res) => {
         res.write('</html>');
         return res.end();
     }
+
     if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
@@ -34,4 +35,15 @@ const requestHandler = (req, res) => {
     res.end();
 };
 
-module.exports = requestHandler;
+//module.exports = requestHandler;
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
+
